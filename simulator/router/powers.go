8d45e054f1acbad944481d245d3d9f53e9fd4b0c
Marko Teiste <mte@iki.fi>
2024-05-26 16:51:27 +0300

    executor: use running as the path to save started program

--- simulator/router/powers.go
+++ simulator/router/powers.go
@@ -2,7 +2,6 @@ package router
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -56,10 +55,8 @@ func operatePower(powers map[string]types.PowerManager) gin.HandlerFunc {
 
 		switch command.Command {
 		case types.PowerOn:
-			log.Printf("Powering on %s to %d%%\n", powerName, command.Percent)
 			power.TurnOn(types.NewCycle(command.Percent))
 		case types.PowerOff:
-			log.Printf("Powering off %s to %d%%\n", powerName, command.Percent)
 			power.TurnOff()
 		default:
 			ctx.JSON(http.StatusBadRequest, types.APIErrorResponse{Err: fmt.Sprintf("Unknown command '%s'", command.Command)})
