0d21219f5f716013331dfa376ffd2d3c8c3c30b5
Marko Teiste <mte@iki>
2024-05-26 11:32:37 +0300

    executor: define ProgramStatus response

--- executor/types/api.go
+++ executor/types/api.go
@@ -1,13 +1,36 @@
 package types
 
-type APIErrorResponse struct {
-	Err string `json:"error"`
-}
+type (
+	APIErrorResponse struct {
+		Err string `json:"error"`
+	}
 
-type APIResponse[T any] struct {
-	Data T `json:"data"`
-}
+	APIResponse[T any] struct {
+		Data T `json:"data"`
+	}
 
-type ProgramListing struct {
-	Programs []string `json:"programs"`
-}
+	ProgramListing struct {
+		Programs []string `json:"programs"`
+	}
+
+	TemperatureStatus struct {
+		Material float32 `json:"material"`
+		Oven     float32 `json:"oven"`
+		Delta    float32 `json:"delta"`
+	}
+
+	PSUStatus struct {
+		Heater     int `json:"heater"`
+		Fan        int `json:"fan"`
+		Humidifier int `json:"humidifier"`
+	}
+
+	ProgramStatus struct {
+		Program              ProgramListing    `json:"program"`
+		StartedAt            int64             `json:"started_at,omitempty"`
+		CurrentStep          string            `json:"current_step,omitempty"`
+		CurrentStepStartedAt int64             `json:"current_step_started_at,omitempty"`
+		Temperatures         TemperatureStatus `json:"temperatures,omitempty"`
+		PowerStatus          PSUStatus         `json:"power_status,omitempty"`
+	}
+)
