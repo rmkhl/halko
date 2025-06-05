ac2224e7a13bcfa45fd822dea501ee007b9dd691
Marko Teiste <mte@iki.fi>
2025-06-05 15:18:42 +0300

    configurator: fix logging of shutdown

--- configurator/main.go
+++ configurator/main.go
@@ -45,7 +45,7 @@ func main() {
 
 	// Attempt graceful shutdown
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatalf("Server forced to shutdown: %v", err)
+		log.Printf("Server forced to shutdown: %v", err)
 	}
 
 	log.Println("Server shutdown complete")
