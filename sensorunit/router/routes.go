a99ce03db59fd212e110221d9e83d31fba91eea9
Marko Teiste <mte@iki.fi>
2025-06-07 15:54:49 +0300

    sensorunit: fix api path

--- sensorunit/router/routes.go
+++ sensorunit/router/routes.go
@@ -8,9 +8,9 @@ import (
 // It follows a similar pattern to powerunit, grouping routes under /api/v1.
 func SetupRoutes(r *gin.Engine, api *API) {
 	sensorsAPI := r.Group("sensors/api")
-	apiV1 := sensorsAPI.Group("sensors/api/v1")
+	apiV1 := sensorsAPI.Group("v1")
 
-	temperatures := apiV1.Group("temperature")
+	temperatures := apiV1.Group("temperatures")
 	temperatures.GET("", api.getTemperatures)
 
 	status := apiV1.Group("status")
