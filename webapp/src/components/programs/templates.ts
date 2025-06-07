4c2aa93dfca123af48e261cd6d7f32e40ce38c0f
pqkallio <pqkallio@users.noreply.github.com>
2025-06-07 22:32:26 +0300

    power configuration for hardware units on program step form

--- webapp/src/components/programs/templates.ts
+++ webapp/src/components/programs/templates.ts
@@ -9,12 +9,32 @@ export const emptyProgram = (): Program => ({
       name: "Heat",
       type: "heating",
       targetTemperature: 100,
+      heater: {
+        pid: {},
+        power: 100,
+      },
+      humidifier: {
+        power: 50,
+      },
+      fan: {
+        power: 0,
+      },
     },
     {
       id: uuidv4(),
       name: "Cool down",
       type: "cooling",
       targetTemperature: 30,
+      heater: {
+        pid: {},
+        power: 0,
+      },
+      humidifier: {
+        power: 0,
+      },
+      fan: {
+        power: 0,
+      },
     },
   ],
 });
