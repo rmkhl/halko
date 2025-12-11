256a085d6aedffd809b3e9c7c3fc00b0dae202d0
Marko Teiste <mte@iki.fi>
2025-12-11 21:48:05 +0200

    halko: fix cors

--- controlunit/main.go
+++ controlunit/main.go
@@ -18,7 +18,7 @@ import (
 
 func addCORSHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:1234")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
 		w.Header().Set("Access-Control-Expose-Headers", "Content-Length")
--- powerunit/router/router.go
+++ powerunit/router/router.go
@@ -24,7 +24,7 @@ func New(p *power.Controller, powerMapping map[string]int, idMapping [shelly.Num
 
 func addCORSHeaders(mux *http.ServeMux) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:1234")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
 		w.Header().Set("Access-Control-Expose-Headers", "Content-Length")
